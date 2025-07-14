978ebd67a2c51dccaa72af8dad6110b32b98c3ea
Aman <mr.aman12nov@gmail.com>
2025-07-14 17:23:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,28 +36,6 @@ async function fetchPoke(){
 
 }
 
-function newPokemon(){
-
-    const container = document.createElement('div')
-    document.body.appendChild(container)
-    
-    const img = document.createElement('img')
-    container.appendChild(img)
-    img.src = "./img-2.jpeg"
-    img.style.height = "30%"
-    img.style.width = "auto"
-    img.style.marginTop = "200px"
-    
-    const h2 = document.createElement("h2")
-    document.body.appendChild(h2)
-    h2.innerHTML = "SHUBHAM"
-    
-    const p = document.createElement("p")
-    document.body.appendChild(p)
-    p.innerHTML = ("Pokédex number 50")
-
-}
-
 function errorHandle(){
     const container = document.createElement('div')
     document.body.appendChild(container)
@@ -71,10 +49,7 @@ function errorHandle(){
 const findPoke = document.querySelector('button')
 findPoke.addEventListener("click", () =>{
     const pokemon = document.querySelector('input').value
-    if(pokemon.toLowerCase()=="shubham" || pokemon=="50" || pokemon==50){
-        newPokemon()
-    }
-    else if(Number(pokemon)>1025){
+    if(Number(pokemon)>1025){
         errorHandle()
     }
     else{
